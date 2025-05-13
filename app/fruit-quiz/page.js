47b3a7bf003dc0c15919f7fbb5576b680653bcae
Jordan Yee <jordanyee2003@gmail.com>
2025-05-14 00:33:33 +1200

    Make Fruit Quiz responsive to smaller screen sizes

--- app/fruit-quiz/page.js
+++ app/fruit-quiz/page.js
@@ -2,7 +2,9 @@ export default function FruitQuiz() {
     return (
         <div className="center-container">
             <h1>Fruit Quiz!</h1>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc_DPonPxNGotm7DsnAFZvlLxij9DGSdMPtEpFy1DVNYxZMcw/viewform?embedded=true" width="640" height="500" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+            <div className="iframe-container">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc_DPonPxNGotm7DsnAFZvlLxij9DGSdMPtEpFy1DVNYxZMcw/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0" allowFullScreen title="Fruit Quiz">Loading…</iframe>
+            </div>
         </div>
     );
 }

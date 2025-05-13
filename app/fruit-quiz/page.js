ab25d305b4389e94b5137aa1f5a151757233c2fd
Jordan Yee <jordanyee2003@gmail.com>
2025-05-14 00:00:52 +1200

    Embed Fruit Quiz Google Form

--- app/fruit-quiz/page.js
+++ app/fruit-quiz/page.js
@@ -2,6 +2,7 @@ export default function FruitQuiz() {
     return (
         <div className="center-container">
             <h1>Fruit Quiz!</h1>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc_DPonPxNGotm7DsnAFZvlLxij9DGSdMPtEpFy1DVNYxZMcw/viewform?embedded=true" width="640" height="500" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
         </div>
     );
 }
